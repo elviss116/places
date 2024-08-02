7807531a541238b27aadc3b1d3b19a9ea0ad9e6e
emiranda116 <emiranda.peruapps@gmail.com>
2024-08-02 10:21:45 -0500

    se agrega splash

---
+++ app/src/main/java/com/example/places/feature/ui/splash/SplashActivity.kt
@@ -0,0 +1,34 @@
+package com.example.places.feature.ui.splash
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import com.example.places.R
+import com.example.places.feature.ui.main.MainActivity
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        lifecycleScope.launch {
+            delay(3000)
+            startActivity(Intent(this@SplashActivity,MainActivity::class.java))
+            finish()
+        }
+    }
+
+}
